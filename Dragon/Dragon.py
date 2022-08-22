6d95e264570995ba4bf46f994040079075cb5fcf
aryan-megatrn <111707745+aryan-megatrn@users.noreply.github.com>
2022-08-22 16:05:56 +0430

    Update and rename default to Dragon.py

---
+++ Dragon/Dragon.py
@@ -0,0 +1,28 @@
+import random
+from random import randint
+import string
+import uuid
+import os
+import time 
+from time import sleep
+from colorama import Fore
+import sys
+###########
+print("\n"*10)
+soheil = """
+\033[41m[1]\33[1;0m  \033[32m Banner Computer1\033[41m
+\033[41m[2]\33[1;0m  \033[32m Banner Computer2
+"""
+for i in soheil:
+	sleep(0.05)
+	print(i,end='',flush=True)
+print()
+
+shah = int(input(Fore.CYAN+"     \033[41m[?]\33[1;0m"+"\33[1; \033[31mSelect\033[32m◈─━─━─━─━\033[34m❯❯❯  \033[93m"+Fore.RED+""))
+#
+if(str(shah) == "1"):
+  os.system("cd Computer1&&bash install.sh")
+#
+if(str(shah) == "2"):
+  os.system("cd Computer2&&bash install.sh
+Footer
