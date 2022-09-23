8575c3ef7a50433069c11d4def851e3181e64bf9
aryan-megatrn <111707745+aryan-megatrn@users.noreply.github.com>
2022-09-24 02:06:13 +0330

    Create uninstall.sh

---
+++ shooter/uninstall.sh
@@ -0,0 +1,21 @@
+
+clear
+read -p " Does you want to install tem-shooter Shell in Termux?(Yes/No) : " input
+
+if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
+    clear
+    cp bash.bashrc $HOME
+    cd /data/data/com.termux/files/usr/etc
+    rm -rf motd
+    rm -rf bash.bashrc
+    cd $HOME
+    mv bash.bashrc /data/data/com.termux/files/usr/etc
+    echo -e "\e[1;91mInstall Shod dadash"
+    echo -e "Restart Termux"
+    exit  3
+elif [[ $input == No || $input == no || $input == n || $input == N ]]; then
+exit 2
+else
+echo -e "\e[1;91mInvalid Option"
+exit 1
+fi
